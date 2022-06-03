8809fa5128d25cf371e96e4b7c58cb0f3fb06a58
peterglezroa <peterglezroa@gmail.com>
2022-06-03 12:00:28 -0500

    Camera try

--- src/camera_run.py
+++ src/camera_run.py
@@ -3,17 +3,29 @@ run_camera.py
 -------------
 Script to run model with the connected camera
 """
+import time
 import numpy as np
 import cv2
+from face_recognition import get_recognition_model, RECOGNITION_MODELS
+from keras_vggface.vggface import VGGFace
+from scipy.ndimage import zoom
+from tensorflow.keras.models import Model
 
 def main():
     # Detect Faces
     # Check more cascades in the cv2 folder!!!
-    face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontal_face_alt2.xml")
+    face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt2.xml")
 
     # Video capture
-    cap = cv2.VideCapture(0)
+    cap = cv2.VideoCapture(0)
 
+    vggmodel = VGGFace(model="vgg16")
+    conv_output = vggmodel.get_layer("fc7").output
+    pred_output = vggmodel.get_layer("fc8/softmax").output
+    vggmodel = Model(vggmodel.input, outputs=[conv_output, pred_output])
+    print(vggmodel.summary())
+
+    frame_time = time.time()
     while (True):
         # Capture frame-by-frame from camera
         ret, frame = cap.read()
@@ -24,12 +36,25 @@ def main():
 
         # Process ROI
         for (x, y, w, h) in faces:
-            roi = frame[y:y+h, x:x+w]
-
-            # Recognize face
-
             # Draw rectangle
             cv2.rectangle(frame, (x, y), (x+w, y+h), [0,255,0], 2)
+
+            if (time.time() - frame_time) >= 10:
+                frame_time = time.time()
+                roi = frame[y:y+h, x:x+w]
+    #            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                roi = cv2.resize(roi, [224,224], interpolation = cv2.INTER_AREA)
+                roi = np.expand_dims(roi, axis=0).astype(np.float32)
+
+                # Recognize face
+                conv, pred = vggmodel.predict(roi)
+                target = np.argmax(pred, axis=1).squeeze()
+                w, b = vggmodel.get_layer("fc8/softmax").weights
+                weights = w[:, target].numpy()
+                heatmap = conv.squeeze() @ weights
+                cv2.imshow("face", zoom(conv, zoom=[224,224]) )#, cmap="jet", alpha=0.5)
+
+#            cv2.imshow("face", roi)
 
         # Display result
         cv2.imshow("frame", frame)
